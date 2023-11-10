6acb08a87642872c2db56685c7182a6e134f70fd
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2023-11-11 00:31:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ http.createServer(function (req, res) {
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+
+const fetchTimeout = (url, ms, { signal, ...options } = {}) => {
+    const controller = new AbortController();
+    const promise = fetch(url, { signal: controller.signal, ...options });
+    if (signal) signal.addEventListener("abort", () => controller.abort());
+    const timeout = setTimeout(() => controller.abort(), ms);
+    return promise.finally(() => clearTimeout(timeout));
+};
+
+setTimeout(() => {
+    const controller = new AbortController();
+
+    console.log("Checking if bot is alive...");
+    fetchTimeout("https://discord-bot-1ozw.onrender.com", 60000).then(res => res.text()).then(data => console.log("Website is up!: " + data)).catch(err => console.log("Website is down!: " + err));
+}, 300000);
